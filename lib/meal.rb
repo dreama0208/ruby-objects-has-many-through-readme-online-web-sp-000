a15cbe764513cb0e83fbf23c9dda4e5001e84c7e
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-16 02:24:41 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
 class Meal
 
+  attr_accessor :water, :custormer, :total, :tip
+
+  @@all = []
+
+  def initialize(waiter, custormer, total, tip = 0)
+    @waiter = waiter
+    @custormer = custormer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
 end
